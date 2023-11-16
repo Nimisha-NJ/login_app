903816f8cb65a2265d21d49a2525becaa903a029
Ajith Joseph C <ajith.joseph@altd.in>
2023-11-16 17:31:33 +0530

    first commit

---
+++ profile.js
@@ -0,0 +1,13 @@
+// const getRandomUrl = () => {
+//   const apiUrl = "https://i.pravatar.cc/300";
+// const randomUserId = Math.floor(Math.random() * 1000) + 1;
+//  const imageUrl = `${apiUrl}?u=${randomUserId}`;    
+//   return imageUrl;
+// };
+
+// const displayImage = () => {
+//   const userImage = document.getElementById("userImage");
+//   userImage.src = getRandomUrl();
+// };
+
+// displayImage();
